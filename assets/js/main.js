7c864a2fbb3d5daf80ff7c0f1b582bdfe8712ce5
munyua <mrmunyuajm@gmail.com>
2026-06-24 10:54:35 +0300

    Add change-background image animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,33 @@ function updateQuote() {
 
 updateQuote();
 setInterval(updateQuote, 10000);
+
+// Background Image Rotation
+const container = document.querySelector(".container");
+const images = [
+  "river1.webp",
+  "river2.webp",
+  "river3.webp",
+  "river4.webp",
+  "river5.webp",
+  "river6.webp",
+  "river7.webp",
+  "river8.webp",
+  "river9.webp",
+  "river10.webp",
+];
+
+let currentImageIndex = 0;
+
+function changeBackgroundImage() {
+  container.classList.remove("fade-in");
+
+  setTimeout(() => {
+    container.style.backgroundImage = `url(assets/images/${images[currentImageIndex]})`;
+    container.classList.add("fade-in");
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+  }, 50);
+}
+
+changeBackgroundImage();
+setInterval(changeBackgroundImage, 10000);
